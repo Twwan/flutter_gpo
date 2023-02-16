d842b70442ed32343a9be735a9e66d7f0f616adb
Twwan <65574497+Twwan@users.noreply.github.com>
2023-02-16 18:47:31 +0700

    Update hw1.dart

--- hw1/hw1.dart
+++ hw1/hw1.dart
@@ -6,7 +6,7 @@
 
 void main() {
   for (int n = 1; n <= 100; n++) {
-    if ((n % 3 == 0) && (n % 5 == 0)) print('Super Quiz');
+    if (n % 3 == 0 && n % 5 == 0) print('Super Quiz');
     else if (n % 3 == 0) print('Super');
     else if (n % 5 == 0) print('Quiz');
     else print(n);
