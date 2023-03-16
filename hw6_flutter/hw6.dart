fcc87a82427a7f7d83b85b39eb1695f2890579b6
Twwan <65574497+Twwan@users.noreply.github.com>
2023-03-16 20:39:27 +0700

    Update hw6.dart

--- hw6_flutter/hw6.dart
+++ hw6_flutter/hw6.dart
@@ -1,29 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.black87),
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black87,
-            title: Text(
-              'AppBar',
-              style: TextStyle(
-                  color: Colors.white, fontSize: 34, fontFamily: 'Roboto'),
-            ),
-            centerTitle: true,
-          ),
-          body: Center(
-            child: Text(
-              'HELLO FLUTTER',
-              style: TextStyle(
-                  color: Colors.purple, fontSize: 54, fontFamily: 'Roboto'),
-            ),
-          )),
-    );
-  }
+void main() {
+  runApp(
+    Center(
+      child: Text("Hello Flutter", textDirection: TextDirection.ltr),
+    ),
+  );
 }
